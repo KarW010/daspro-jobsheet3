05731aa41934a2d941a00b9bc7915ab0175a55f2
KarW010 <kartikostudy@gmail.com>
2024-09-14 08:30:50 +0700

    nilai akhir

--- Siakad09.java
+++ Siakad09.java
@@ -2,30 +2,19 @@ import java.util.Scanner;
 public class Siakad09 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        String nama, nim;
-        char kelas;
-        byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        int nilaiKuis, nilaiTugas, nilaiUTS, nilaiUAS;
+        double nilaiAkhir;
 
-        System.out.print("Masukkan nama: ");
-        nama = sc.nextLine();
-        System.out.print("Masukkan NIM: ");
-        nim = sc.nextLine();
-        System.out.print("Masukkan kelas: ");
-        kelas = sc.nextLine().charAt(0);
-        System.out.print("Masukkan nomor absen: ");
-        absen = sc.nextByte();
         System.out.print("Masukkan nilai kuis: ");
-        nilaiKuis = sc.nextDouble();
+        nilaiKuis = sc.nextInt();
         System.out.print("Masukkan nilai tugas:");
-        nilaiTugas = sc.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = sc.nextDouble();
+        nilaiTugas = sc.nextInt();
+        System.out.print("Masukkan nilai UTS: ");
+        nilaiUTS = sc.nextInt();
+        System.out.print("Masukkan nilai UAS: ");
+        nilaiUAS = sc.nextInt();
 
-        nilaiAkhir = (nilaiKuis + nilaiTugas + nilaiUjian) / 3;
-
-        System.out.println("Nama: " + nama + " NIM: " + nim);
-        System.out.println("Kelas " + kelas + " Absen: " + absen);
+        nilaiAkhir = (nilaiKuis * 0.2) + (nilaiTugas * 0.15) + (nilaiUTS * 0.3) + (nilaiUAS * 0.35);
         System.out.println("Nilai Akhir: " + nilaiAkhir);
 
     }
